9fbcbd49363f5585e7eaf977eb3727c33c120b6c
AARONPOWDERLY-STUDEN <G00434425@atu.ie>
2025-10-01 15:47:48 +0100

    Added feature validation-basic

--- src/main/java/ie/atu/week_3cicd/Product.java
+++ src/main/java/ie/atu/week_3cicd/Product.java
@@ -1,11 +1,16 @@
 package ie.atu.week_3cicd;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class Product {
+    @NotBlank
     private String productName;
+    @Positive
     private double price;
 }
